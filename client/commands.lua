235379b9ddfa10c4b31a17ef0dd8a94016c97be9
vipex <101529155+vipexv@users.noreply.github.com>
2024-02-03 18:37:02 +0100

    revert: remove dv/car/door commands, forgot to remove, i used it for debugging

--- client/commands.lua
+++ client/commands.lua
@@ -4,47 +4,47 @@ RegisterCommand(Config.CommandName, function()
     Debug("[command:show-nui] ToggleNuiFrame called and set to true.")
 end, false)
 
-RegisterCommand("car", function(source, args, rawCommand)
-    local carModel = args[1]
+-- RegisterCommand("car", function(source, args, rawCommand)
+--     local carModel = args[1]
 
-    local ped = PlayerPedId()
-    local pedCoords = GetEntityCoords(ped)
-    local pedHeading = GetEntityHeading(ped)
-    local isInVeh = IsPedInAnyVehicle(ped, false)
+--     local ped = PlayerPedId()
+--     local pedCoords = GetEntityCoords(ped)
+--     local pedHeading = GetEntityHeading(ped)
+--     local isInVeh = IsPedInAnyVehicle(ped, false)
 
-    if isInVeh then
-        local currVeh = GetVehiclePedIsIn(ped, false)
+--     if isInVeh then
+--         local currVeh = GetVehiclePedIsIn(ped, false)
 
-        SetEntityAsNoLongerNeeded(currVeh)
-        SetModelAsNoLongerNeeded(currVeh)
+--         SetEntityAsNoLongerNeeded(currVeh)
+--         SetModelAsNoLongerNeeded(currVeh)
 
-        DeleteEntity(currVeh)
-    end
+--         DeleteEntity(currVeh)
+--     end
 
-    RequestModel(carModel)
+--     RequestModel(carModel)
 
-    while not HasModelLoaded(carModel) do Wait(0) end
+--     while not HasModelLoaded(carModel) do Wait(0) end
 
-    local veh = CreateVehicle(carModel, pedCoords.x, pedCoords.y, pedCoords.z, pedHeading, true, false)
+--     local veh = CreateVehicle(carModel, pedCoords.x, pedCoords.y, pedCoords.z, pedHeading, true, false)
 
-    SetPedIntoVehicle(ped, veh, -1)
-end, false)
-
-
-RegisterCommand("dv", function(source, args, rawCommand)
-    local ped = PlayerPedId()
-    local currVeh = GetVehiclePedIsIn(ped, false)
-
-    SetEntityAsNoLongerNeeded(currVeh)
-    SetModelAsNoLongerNeeded(currVeh)
-
-    DeleteEntity(currVeh)
-end, false)
+--     SetPedIntoVehicle(ped, veh, -1)
+-- end, false)
 
 
-RegisterCommand("door", function(source, args, rawCommand)
-    local ped = PlayerPedId()
-    local door = args[1]
-    local currVeh = GetVehiclePedIsIn(ped, false)
-    SetVehicleDoorOpen(currVeh, tonumber(door), false, false)
-end, false)
+-- RegisterCommand("dv", function(source, args, rawCommand)
+--     local ped = PlayerPedId()
+--     local currVeh = GetVehiclePedIsIn(ped, false)
+
+--     SetEntityAsNoLongerNeeded(currVeh)
+--     SetModelAsNoLongerNeeded(currVeh)
+
+--     DeleteEntity(currVeh)
+-- end, false)
+
+
+-- RegisterCommand("door", function(source, args, rawCommand)
+--     local ped = PlayerPedId()
+--     local door = args[1]
+--     local currVeh = GetVehiclePedIsIn(ped, false)
+--     SetVehicleDoorOpen(currVeh, tonumber(door), false, false)
+-- end, false)
